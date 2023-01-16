5272e74f19f93cd5a6d59ba674bb62725c409fb2
Sulav Neupane <neupsulav@gmail.com>
2023-01-16 21:23:13 +0545

    Added querySelectorAll

--- calculator/script.js
+++ calculator/script.js
@@ -1,14 +1,25 @@
 let screen = document.getElementById('screen')
-let buttons = document.getElementsByClassName('btn')
+// let buttons = document.getElementsByClassName('btn')
+
+//method-2
+let buttons = document.querySelectorAll('.btn')
 let clearBtn = document.getElementById('clear')
 let equalBtn = document.getElementById('equal')
 
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', () => {
-        let value = buttons[i].dataset.num
+// for (let i = 0; i < buttons.length; i++) {
+//     buttons[i].addEventListener('click', () => {
+//         let value = buttons[i].dataset.num
+//         screen.value += value
+//     })
+// }
+
+// method-2
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let value = button.dataset.num
         screen.value += value
     })
-}
+});
 
 equalBtn.addEventListener('click', () => {
     var answer = screen.value
