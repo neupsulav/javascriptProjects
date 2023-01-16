09c1f9b2b18d2f0e4c7a93d364d6fc8ca2a40bc2
Sulav Neupane <neupsulav@gmail.com>
2023-01-16 21:46:30 +0545

    Improved some JS logic

--- productSearch/script.js
+++ productSearch/script.js
@@ -7,16 +7,31 @@ productSearch.addEventListener('keyup', () => {
     let searchValue = productSearch.value.toUpperCase()
     // accessing the classname productItem to manipulate its display property later 
     let productItem = document.getElementsByClassName('productItem')
-    // accessing the product title to perform search function 
-    let productTitle = document.getElementsByClassName('productTitle')
 
-    for (let i = 0; i < productTitle.length; i++) {
-        let productTitleValues = productTitle[i].innerText.toUpperCase()
-        if (productTitleValues.indexOf(searchValue) > -1) {
-            productItem[i].style.display = ''
+    // method 1
+    // accessing the product title to perform search function
+    // let productTitle = document.getElementsByClassName('productTitle')
+
+    // for (let i = 0; i < productTitle.length; i++) {
+    //     let productTitleValues = productTitle[i].innerText.toUpperCase()
+    //     if (productTitleValues.indexOf(searchValue) > -1) {
+    //         productItem[i].style.display = ''
+    //     } else {
+    //         productItem[i].style.display = 'none'
+    //     }
+    // }
+
+    // method-2
+    let productTitle = document.querySelectorAll('.productTitle')
+    let index = 0
+    productTitle.forEach((title) => {
+        let productTitleValue = title.innerText.toUpperCase()
+        if (productTitleValue.indexOf(searchValue) > -1) {
+            productItem[index].style.display = ''
         } else {
-            productItem[i].style.display = 'none'
+            productItem[index].style.display = 'none'
         }
-    }
+        index++
+    })
 
 })
